250ff43ed183b85abff0d6a99008334541850019
Kaobara <dannymahmud1@gmail.com>
2023-02-04 20:21:22 +0700

    Simple Discord bot implementation and use
    fixed dependency problems.
    
    Simple bot can go online on Discord

--- src/main/java/com/gradle/discord4jDiscordBot/simpleBot.java
+++ src/main/java/com/gradle/discord4jDiscordBot/simpleBot.java
@@ -1,10 +1,15 @@
 package com.gradle.discord4jDiscordBot;
 
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import reactor.core.publisher.Mono;
+
 public class simpleBot {
     public static void main(String[] args) {
-        Mono.just("Hello World").subscribe(System.out::println);
-// or
-        Flux.just('H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '\n')
-                .subscribe(System.out::print);
+        DiscordClient client = DiscordClient.create("OTM1NzgwODI4MzY0MDI5OTky.G96VpA.jsYhiQczCvBnzCSB45UNYqjjwgP8F6rxatZg80");
+
+        Mono<Void> login = client .withGateway((GatewayDiscordClient gateway) -> Mono.empty());
+
+        login.block();
     }
 }
