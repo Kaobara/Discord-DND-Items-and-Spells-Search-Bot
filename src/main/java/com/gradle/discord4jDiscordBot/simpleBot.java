9911f81a77e9da855ca7d061e2162c10aa4601f8
Kaobara <dannymahmud1@gmail.com>
2023-02-09 03:28:26 +0700

    Token Name :(

--- src/main/java/com/gradle/discord4jDiscordBot/simpleBot.java
+++ src/main/java/com/gradle/discord4jDiscordBot/simpleBot.java
@@ -17,7 +17,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class simpleBot {
     public static void main(String[] args) {
-        GatewayDiscordClient client = DiscordClientBuilder.create("OTM1NzgwODI4MzY0MDI5OTky.GltlmB.micSl_QqIic0DAPNEnCvHA_ZPrrlhv9yiF2vRY")
+        GatewayDiscordClient client = DiscordClientBuilder.create("Token Here")
                 .build()
                 .login()
                 .block();
